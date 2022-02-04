873d2b1fe37b338f9d77d18bafcb3706f7263a3b
Parth Shah <parth.shah2118@gmail.com>
2022-02-04 17:17:28 -0500

    first commit

--- server.ts
+++ server.ts
@@ -2,10 +2,10 @@ import express, {Request, Response} from 'express';
 import mongoose from "mongoose";
 mongoose.connect('mongodb+srv://parth2118:<password>@cluster0.stuva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 import bodyParser from "body-parser";
-
+import CourseController from "./controllers/CourseController";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
-
+import LikeController from "./controllers/LikeController";
 const app = express();
 app.use(bodyParser.json())
 
@@ -15,10 +15,10 @@ app.get('/', (req: Request, res: Response) =>
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
 
-
+const courseController = new CourseController(app);
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
-
+const likesController = LikeController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
